f0db8ca1485c37a9658895f7de59d84eb5a62d8f
Christian Jimenez <jimenex0608@gmail.com>
2023-04-19 17:20:13 -0500

    Latest changes: Cursor customized

--- script.js
+++ script.js
@@ -64,7 +64,36 @@ const obsSectionOne = new IntersectionObserver(
   {
     root: null,
     threshold: 0,
-    rootMargin: "-150px",
+    rootMargin: "-30px",
   }
 );
 obsSectionOne.observe(sectionOneEl);
+
+///////////////////////////////////////////////////////////////////
+// CURSOR
+
+const innerCursor = document.querySelector(".inner-cursor");
+const outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  innerCursor.style.left = `${x}px`;
+  innerCursor.style.top = `${y}px`;
+  outerCursor.style.left = `${x}px`;
+  outerCursor.style.top = `${y}px`;
+}
+
+const links = Array.from(document.querySelectorAll("a"));
+
+links.forEach(link => {
+  link.addEventListener("mouseover", () => {
+    innerCursor.classList.add("grow");
+  });
+  link.addEventListener("mouseleave", () => {
+    innerCursor.classList.remove("grow");
+  });
+});
